6d54bcb5ce1e6a6f8fe8b6f40dc5b78e36b68047
Komal Meena <80938016+komalmeena6516@users.noreply.github.com>
2023-02-11 10:49:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,48 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(a => {
+    if (a.marks > 50) {
+     console.log(a);
+    }
+  });
+
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(a => {
+    if (a.marks > 50) {
+      console.log(a);
+    }
+  });
+
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newArr = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newArr);
+  console.log(arr);
+
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let filteredArr = arr.filter(a => a.marks >= 50);
+  console.log(filteredArr);
+
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 = [
+    { id: 7, name: "komal", age: "20", marks: 85 },
+    { id: 9, name: "Aman-Tiwari", age: "20", marks: 85 },
+    { id: 11, name: "Sonam Meena", age: "20", marks: 85 },
+  ];
+  let all = arr.concat(arr1);
+console.log(all);
+
+  
 }
